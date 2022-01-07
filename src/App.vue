596dc5a0972b70798c9860da45414729a7f242b0
Ejiofor Olisaemeka <olisehemeke37@gmail.com>
2022-01-07 19:22:06 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container">
+      <AddTodo />
+      <FilterTodos />
+      <Todos />
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+import Todos from './components/Todos.vue'
+import AddTodo from './components/AddTodos.vue'
+import FilterTodos from './components/Filter.vue'
+
+
+export default {
+  name: 'app',
+  components: {
+    Todos,
+    AddTodo,
+    FilterTodos,
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body{
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.container{
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
 }
 </style>
